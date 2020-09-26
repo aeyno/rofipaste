559aad0a4989d05ad95440e184432e31cfbe4986
Any0ne22 <tom.gouville@protonmail.com>
2020-09-26 15:54:09 +0200

    implementing @ValekoZ work

--- rofipaste.py
+++ rofipaste.py
@@ -12,97 +12,96 @@ import configargparse
 from xdg import BaseDirectory
 
 
-thingsToPaste = []
+folder_icon: str = ""
+paste_icon: str = ""
+undo_icon: str = ""
 
 
-def loadConfig(configFile):
-    global thingsToPaste
-    pastesFile = open("./pastes.json")
-    thingsToPaste = json.loads(pastesFile.read())
-    pastesFile.close()
+def read_folder_content(folder_path: str) -> str:
+    file_entries = ''
+    dir_entries = ''
 
-
-def getThingsToPaste(commandList) -> str:
-    params = [":edit"]
-    for x in commandList:
-        if x["type"] == "directory":
-            params.append("📂"+x["name"])
+    for f in os.listdir(folder_path):
+        if os.path.isfile(os.path.join(folder_path, f)):
+            file_entries += f'{paste_icon} {f}\n'
         else:
-            params.append(x["name"])
-    return "\n".join(params)
+            dir_entries += f'{folder_icon} {f}\n'
+
+    return file_entries + dir_entries
 
 
-def commandInterpreter(command):
-    global thingsToPaste
-    if(command == ":edit"):
-        editConfig("./pastes.json")
-        return ""
+def fileInterpreter(path):
+    fileExt = path.split(".")[-1]
+    print(fileExt)
+    if fileExt == "sh":
+        #TODO : regex to parse spaces in the command
+        cmd = run(["bash", path],capture_output=True,encoding='utf-8')
+        return cmd.stdout
     else:
-        for x in thingsToPaste:
-            if x["name"] == command:
-                if x["type"] == "directory":
-                    # TODO: Launch rofi again with the content of the folder
-                    pass
-                elif x["type"] == "text":
-                    return x["value"]
-                elif x["type"] == "command":
-                    #TODO : regex to parse spaces in the command
-                    cmd = run(x["value"].split(" "),capture_output=True,encoding='utf-8')
-                    print(cmd.stdout)
-                    return cmd.stdout
-
-
-def editConfig(configFile):
-    run(args=['vim', configFile,], encoding='utf-8')
-    try:
-        loadConfig(configFile)
-        print("Changes saved !")
-    except:
-        print("Something is wrong with your file !")
+        f = open(path, 'r')
+        content = f.read()
+        f.close()
+        return content
 
 
 def main() -> None:
-    global thingsToPaste
-    loadConfig("./pastes.json")
     args = parse_arguments()
     active_window = get_active_window()
+    
+    #base_folder = os.path.join(BaseDirectory.xdg_data_home, 'rofipaste',args['files'])
+    #current_folder = os.path.join(BaseDirectory.xdg_data_home, 'rofipaste',args['files'])
 
-    returncode, stdout = open_main_rofi_window(
-        args.rofi_args,
-        #read_character_files(args.files),
-        getThingsToPaste(thingsToPaste),
-        args.prompt,
-        args.max_recent
-    )
+    base_folder = "./pastes_folder"
+    current_folder = base_folder
 
-    if returncode == 1:
-        sys.exit()
-    else:
-        if 10 <= returncode <= 19:
-            pass
-            # TODO: create shortcuts with 0-9 keys
-            #default_handle_recent_character(returncode - 9, args, active_window)
-        else:
-            cmd = stdout.splitlines()[0]
-            print(cmd)
-            data = commandInterpreter(cmd)
-            if returncode == 0:
-                default_handle(data, args, active_window)
-            elif returncode == 20:
-                copy_characters_to_clipboard(data)
-            elif returncode == 21:
-                type_characters(data, active_window)
-            elif returncode == 22:
-                copy_paste_characters(data, active_window)
+    while True:
+        folder_content = read_folder_content(current_folder)
+
+        if current_folder != base_folder:
+            folder_content = f'{undo_icon} ..\n' + folder_content
+        
+        returncode, stdout = open_main_rofi_window(
+            args.rofi_args,
+            folder_content,
+            args.prompt,
+            args.max_recent
+        )
+
+        if returncode == 1:
+            sys.exit()
+        
+        splitted = stdout.rstrip('\n').split(' ')
+        icon, path = splitted[0], os.path.join(current_folder,' '.join(splitted[1:]))
+
+        if icon == folder_icon:
+            current_folder = path
+        elif icon == undo_icon:
+            current_folder = os.path.dirname(current_folder)
+        elif icon == paste_icon:
+            if 10 <= returncode <= 19:
+                # TODO: create shortcuts with 0-9 keys
+                #default_handle_recent_character(returncode - 9, args, active_window)
+                return
+            else:
+                data = fileInterpreter(path)
+                if returncode == 0:
+                    default_handle(data, args, active_window)
+                elif returncode == 20:
+                    copy_characters_to_clipboard(data)
+                elif returncode == 21:
+                    type_characters(data, active_window)
+                elif returncode == 22:
+                    copy_paste_characters(data, active_window)
+                return
 
 
 def parse_arguments() -> argparse.Namespace:
     parser = configargparse.ArgumentParser(
-        description='Select, insert or copy Unicode characters using rofi.',
-        default_config_files=[os.path.join(directory, 'rofimoji.rc') for directory in
+        description='Paste text using rofi.',
+        default_config_files=[os.path.join(directory, 'rofipast.rc') for directory in
                               BaseDirectory.xdg_config_dirs]
     )
-    parser.add_argument('--version', action='version', version='rofimoji 4.2.0')
+    parser.add_argument('--version', action='version', version='rofipaste 0.1')
     parser.add_argument(
         '--insert-with-clipboard',
         '-p',
@@ -118,22 +117,13 @@ def parse_arguments() -> argparse.Namespace:
         action='store_true',
         help='Only copy the character to the clipboard but do not insert it'
     )
-    parser.add_argument(
-        '--skin-tone',
-        '-s',
-        dest='skin_tone',
-        action='store',
-        choices=['neutral', 'light', 'medium-light', 'moderate', 'dark brown', 'black', 'ask'],
-        default='ask',
-        help='Decide on a skin-tone for all supported emojis. If not set (or set to "ask"), '
-             'you will be asked for each one '
-    )
+
     parser.add_argument(
         '--files',
         '-f',
         dest='files',
         action='store',
-        default=['emojis'],
+        default='pastes_folder',
         nargs='+',
         metavar='FILE',
         help='Read characters from this file instead, one entry per line'
@@ -144,7 +134,7 @@ def parse_arguments() -> argparse.Namespace:
         dest='prompt',
         action='store',
         default='Rofipaste ❤ ',
-        help='Set rofimoj\'s  prompt'
+        help='Set rofipast\'s  prompt'
     )
     parser.add_argument(
         '--rofi-args',
